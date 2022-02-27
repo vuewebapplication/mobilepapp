859187e3fc7249d3ed423fda4f2cb466a44719b0
vuewebapplication <mayjanequeen@gmail.com>
2022-02-27 23:36:27 +0800

    add production

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -45,7 +45,7 @@
     >
       <v-row align="center" justify="center">
         <v-col class="text-center" cols="12">
-          <h1 class="text-h4 font-weight-thin mb-4">PHP 1000.00</h1>
+          <h1 class="text-h4 font-weight-thin mb-4">PHP 1000.00 test</h1>
           <h4 class="subheading">Winning Prices</h4>
         </v-col>
       </v-row>
