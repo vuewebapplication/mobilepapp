f5837cc9f249a535ad2ad90bff94ae6c52955b56
vuewebapplication <mayjanequeen@gmail.com>
2022-03-06 21:09:07 +0800

    update layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,24 @@
 <template>
   <v-container class="maincontainer" fluid>
     <v-navigation-drawer v-model="drawer" temporary app>
-      <v-list-item>
+      <v-divider></v-divider>
+      <v-toolbar color="#6200ea" dark flat>
+        <v-toolbar-title >Your Dashboard</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon >
+          <v-icon>{{ icons.mdiEmailOutline }}</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-list-item two-line>
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/women/81.jpg" />
+        </v-list-item-avatar>
+
         <v-list-item-content>
-          <v-list-item-title class="text-h6"> Application </v-list-item-title>
-          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+          <v-list-item-title>Jucel Estribo</v-list-item-title>
+          <v-list-item-subtitle>Logged In</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
@@ -55,86 +69,82 @@
           <h4 class="subheading">Winning Prize</h4>
         </v-card>
         <v-container fluid>
+          <v-sheet>
+            <v-responsive>
+              <v-row dense class="mx-auto imgbox">
+                <v-col
+                  v-for="card in cards"
+                  :key="card.title"
+                  :cols="card.flex"
+                >
+                  <v-card>
+                    <v-img
+                      :src="card.src"
+                      class="white--text align-end"
+                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                      height="200px"
+                    >
+                      <v-card-title v-text="card.title"></v-card-title>
+                    </v-img>
 
-        <v-sheet>
-          <v-card class="mx-auto imgbox" max-width="500">
-            <v-row dense>
-              <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
-                <v-card>
-                  <v-img
-                    :src="card.src"
-                    class="white--text align-end"
-                    gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                    height="200px"
-                  >
-                    <v-card-title v-text="card.title"></v-card-title>
-                  </v-img>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
 
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
+                      <v-btn icon>
+                        <v-icon>mdi-heart</v-icon>
+                      </v-btn>
 
-                    <v-btn icon>
-                      <v-icon>mdi-heart</v-icon>
-                    </v-btn>
+                      <v-btn icon>
+                        <v-icon>mdi-bookmark</v-icon>
+                      </v-btn>
 
-                    <v-btn icon>
-                      <v-icon>mdi-bookmark</v-icon>
-                    </v-btn>
+                      <v-btn icon>
+                        <v-icon>mdi-share-variant</v-icon>
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-responsive>
+          </v-sheet>
 
-                    <v-btn icon>
-                      <v-icon>mdi-share-variant</v-icon>
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-card>
-          <v-responsive min-height="100vh"> </v-responsive>
-        </v-sheet>
+          <v-bottom-navigation fixed color="primary" scroll-threshold="500">
+            <v-btn color="accent-4" @click="drawer = !drawer">
+              <span>Home</span>
+              <center>
+                <v-icon>{{ icons.mdiHomeAccount }}</v-icon>
+              </center>
+            </v-btn>
 
+            <v-btn color="accent-4" @click="drawer = !drawer">
+              <span>Member</span>
+              <center>
+                <v-icon>{{ icons.mdiAccountGroupOutline }}</v-icon>
+              </center>
+            </v-btn>
+
+            <v-btn color="accent-4" @click="drawer = !drawer">
+              <span>Tasks</span>
+              <center>
+                <v-icon>{{ icons.mdiCheckboxMarkedCirclePlusOutline }}</v-icon>
+              </center>
+            </v-btn>
+
+            <v-btn color="accent-4" @click="drawer = !drawer">
+              <span>Record</span>
+              <center>
+                <v-icon>{{ icons.mdiBallotRecountOutline }}</v-icon>
+              </center>
+            </v-btn>
+
+            <v-btn color="accent-4" @click="drawer = !drawer">
+              <span>My</span>
+              <center>
+                <v-icon>{{ icons.mdiAccountDetailsOutline }}</v-icon>
+              </center>
+            </v-btn>
+          </v-bottom-navigation>
         </v-container>
-        <v-bottom-navigation
-          fixed
-          color="primary"
-          hide-on-scroll
-          scroll-target="#scroll-threshold-example"
-          scroll-threshold="500"
-        >
-          <v-btn color="accent-4" @click="drawer = !drawer">
-            <span>Home</span>
-            <center>
-              <v-icon>{{ icons.mdiHomeAccount }}</v-icon>
-            </center>
-          </v-btn>
-
-          <v-btn color="accent-4" @click="drawer = !drawer">
-            <span>Member</span>
-            <center>
-              <v-icon>{{ icons.mdiAccountGroupOutline }}</v-icon>
-            </center>
-          </v-btn>
-
-          <v-btn color="accent-4" @click="drawer = !drawer">
-            <span>Tasks</span>
-            <center>
-              <v-icon>{{ icons.mdiCheckboxMarkedCirclePlusOutline }}</v-icon>
-            </center>
-          </v-btn>
-
-          <v-btn color="accent-4" @click="drawer = !drawer">
-            <span>Record</span>
-            <center>
-              <v-icon>{{ icons.mdiBallotRecountOutline }}</v-icon>
-            </center>
-          </v-btn>
-
-          <v-btn color="accent-4" @click="drawer = !drawer">
-            <span>My</span>
-            <center>
-              <v-icon>{{ icons.mdiAccountDetailsOutline }}</v-icon>
-            </center>
-          </v-btn>
-        </v-bottom-navigation>
       </v-card>
     </main>
   </v-container>
@@ -179,9 +189,9 @@ export default {
         },
       ],
       items: [
-        { title: "Home", icon: "mdi-home-city" },
-        { title: "My Account", icon: "mdi-account" },
-        { title: "Users", icon: "mdi-account-group-outline" },
+        { title: "Home", icon: mdiHomeAccount },
+        { title: "My Account", icon: mdiAccountDetailsOutline },
+        { title: "Users", icon: mdiAccountGroupOutline },
       ],
       icons: {
         mdiAccountMultipleCheck,
@@ -213,7 +223,7 @@ export default {
   display: block !important;
   background-color: #6200ea !important;
   border-color: #6200ea !important;
-  z-index: 1000;
+  z-index: 1;
   padding: 15px !important;
 }
 .v-sheet.v-card {
@@ -226,12 +236,8 @@ export default {
 .v-item-group.v-bottom-navigation {
   z-index: 1 !important;
   border-radius: 0px !important;
-
   border: none !important;
   box-shadow: none !important;
-  left: 0 !important;
-  right: 0 !important;
-  bottom: 0 !important;
 }
 .v-item-group.v-bottom-navigation .v-btn {
   background-color: white;
@@ -240,10 +246,16 @@ export default {
   flex: 0 1 auto;
   font-size: 0.75rem;
   height: inherit;
-  max-width: 168px;
-  min-width: 80px;
   position: relative;
   text-transform: none;
+}
+
+.v-item-group.v-bottom-navigation--fixed {
+  bottom: 0 !important;
+  left: 0 !important;
+  width: 27.5rem !important;
+  margin: 0 auto !important;
+  right: 0 !important;
 }
 .v-app-bar--fixed {
   height: 64px;
@@ -253,11 +265,14 @@ export default {
   margin: 0 auto !important;
 }
 .v-navigation-drawer {
-  top: -45px !important;
   margin: 0 auto !important;
 }
 .v-navigation-drawer--fixed {
-  position: absolute !important;
+  position: fixed !important;
+  width: 27.5rem !important;
+  height: 100% !important;
+  bottom: 0px !important;
+  right: 0% !important;
 }
 .v-overlay__scrim {
   top: 0 !important;
@@ -288,8 +303,41 @@ body {
 }
 
 @media (max-width: 960px) {
+  .v-item-group.v-bottom-navigation--fixed {
+    bottom: 0 !important;
+    left: 0 !important;
+    width: 100% !important;
+    margin: 0 auto !important;
+    right: 0 !important;
+  }
+  .v-item-group.v-bottom-navigation .v-btn {
+    background-color: white;
+    border-radius: 0;
+    box-shadow: none;
+    flex: 0 1 auto;
+    font-size: 0.75rem;
+    height: inherit;
+    max-width: 166px !important;
+    min-width: 0px !important;
+    position: relative;
+    text-transform: none;
+  }
+  .v-app-bar--fixed {
+    height: 64px;
+    margin-top: 0px;
+    transform: translateY(0px);
+    width: 27.5rem !important;
+  }
   .v-navigation-drawer {
-    top: -34px !important;
+    margin: 0  !important;
+  }
+  .v-navigation-drawer--fixed {
+    position: fixed !important;
+    width: 70% !important;
+    height: 100% !important;
+    bottom: 0px !important;
+    right: 0% !important;
+    left: 0px !important;
   }
   .v-btn:not(.v-btn--round).v-size--default {
     height: 56px !important;
