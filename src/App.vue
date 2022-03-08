46342f05dddaff0b6d9104828571d054594e5381
vuewebapplication <mayjanequeen@gmail.com>
2022-03-09 01:34:10 +0800

    edit menu

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,33 @@
     <v-main>
       <sidebar></sidebar>
       <nav-title></nav-title>
-       <nav-price></nav-price>
-       <router-view></router-view>
-       <navbar></navbar>
+
+      <main
+        id="scroll-threshold-example"
+        class="overflow-y-auto pb-16"
+        height="95vh"
+      >
+        <v-card class="mx-auto overflow-hidden" max-width="500">
+          <nav-price></nav-price>
+
+          <v-container fluid>
+            <router-view></router-view>
+            <navbar></navbar>
+          </v-container>
+        </v-card>
+      </main>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Sidebar from './layouts/Sidebar.vue';
-import NavTitle from './layouts/NavTitle.vue';
-import NavPrice from './layouts/NavPrice.vue';
-import Navbar from './layouts/Navbar.vue';
+import Sidebar from "./layouts/Sidebar.vue";
+import NavTitle from "./layouts/NavTitle.vue";
+import NavPrice from "./layouts/NavPrice.vue";
+import Navbar from "./layouts/Navbar.vue";
 export default {
-  name:'App',
-  components:{
+  name: "App",
+  components: {
     Sidebar,
     NavTitle,
     NavPrice,
