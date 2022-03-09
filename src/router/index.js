27591671dd4c8b5966c0c2d91580d22ea85eed69
vuewebapplication <mayjanequeen@gmail.com>
2022-03-09 15:52:52 +0800

    edit menu

--- src/router/index.js
+++ src/router/index.js
@@ -16,30 +16,36 @@ const routes = [{
 
     {
         path: '/home',
+        name: 'home',
         component: () =>
             import ('@/views/Home.vue'),
     },
 
     {
         path: '/members',
+        name: 'members',
         component: () =>
             import ('@/views/Members.vue'),
     },
 
     {
         path: '/tasks',
+        name: 'tasks',
         component: () =>
             import ('@/views/Tasks.vue'),
     },
 
     {
         path: '/records',
+        name: 'records',
         component: () =>
             import ('@/views/Records.vue'),
     },
 
     {
         path: '/profile',
+
+        name: 'profile',
         component: () =>
             import ('@/views/Profile.vue'),
     },
